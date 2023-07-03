11dfc096161d95c0d5412d7299f930090c532c2a
Alvin Januar <aljan_r@outlook.co.id>
2023-07-04 00:36:22 +0700

    Usable service

--- Makefile
+++ Makefile
@@ -1,5 +1,35 @@
+SHELL := /bin/bash
+ifneq (,$(wildcard ./.env))
+    include .env
+    export
+endif
+
+clean:
+	sudo rm /tmp/minecraft-server.jar /etc/systemd/system/minecraft-server.service
+	sudo userdel $(MINECRAFT_SERVER_LINUX_USER)
+	rm -rf cache libraries
+
+lint:
+	shellcheck -x *.sh
+
+read_env:
+	set -o allexport; source .env; set +o allexport
+
 setup:
 	cp .env.example .env
 
-install: setup
+install:
 	./install.sh
+
+run-installed: read_env
+	cd /opt/minecraft-server
+	/usr/bin/java \
+		-Xmn$(MINIMUM_MEMORY_LIMIT) \
+		-XX:SoftMaxHeapSize=$(SOFT_MAXIMUM_MEMORY_LIMIT) \
+		-Xmx$(MAXIMUM_MEMORY_LIMIT) \
+		-jar /opt/minecraft-server/minecraft-server.jar \
+		--nogui
+
+uninstall:
+	sudo rm -rf /opt/minecraft-server
+	sudo systemctl disable minecraft-server
--- install.sh
+++ install.sh
@@ -1,22 +1,51 @@
 #!/usr/bin/env bash
 
-if [ ! -f .env ]; then
-    export $(cat .env | xargs)
+if [ -f .env ]; then
+    # shellcheck source=.env
+    set -o allexport; source .env; set +o allexport
 fi
 
 function downloadMinecraftServerFromURLToFile {
-    curl -L $1 -o $2
+    if [ ! -f "$2" ]; then
+        if ! curl -L "$1" -o "$2"; then
+            echo Error in downloading error: code $?.
+            exit 255
+        fi
+    return
+    fi
+    echo A downloaded Minecraft server is found. Let\'s use that instead.
+}
+
+function installServiceToSystemd {
+    sudo /bin/bash <<EOF
+        mkdir -p /opt/minecraft-server
+        cp -t /opt/minecraft-server assets/minecraft-server.service /tmp/minecraft-server.jar
+        chmod -R 644 /opt/minecraft-server
+        ln -s /opt/minecraft-server/minecraft-server.service /etc/systemd/system/minecraft-server.service
+EOF
+}
+
+function createServerUser {
+    if ! grep -q "$1" /etc/passwd; then
+        echo User "$1" not found, creating.
+        sudo useradd "$1"
+        return
+    fi
+    echo User "$1" is found in passwd, skipping.
 }
 
 function main {
-    echo Getting Minecraft server...
-    downloadMinecraftServer $MINECRAFT_SERVER_URL minecraft-server.jar
+    echo Getting Minecraft server from "$MINECRAFT_SERVER_URL"...
+    downloadMinecraftServerFromURLToFile "$MINECRAFT_SERVER_URL" /tmp/minecraft-server.jar
 
     echo Requiring root access...
-    sudo /bin/bash <<EOF
-        cp minecraft-server.service /etc/systemd/systemd
-        sudo chmod 755 /etc/systemd/system/minecraft-server.service
-EOF
+    installServiceToSystemd
+
+    echo Creating a minecraft server user "$MINECRAFT_SERVER_LINUX_USER"...
+    createServerUser "$MINECRAFT_SERVER_LINUX_USER"
+
+    echo Registering a systemd service
+    sudo systemctl enable minecraft-server
 
     echo Donezo.
 }
